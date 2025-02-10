ad981ac4eada407aee569abb27d588f2a7f7b6e9
Jitixaa Sutariya <jitixaasutariya@gmail.com>
2025-02-11 09:55:25 +1100

    add basic project

---
+++ Card-Basic/src/App.jsx
@@ -0,0 +1,10 @@
+export const App = () => {
+  return (
+  <div>
+    <img src="queen_tear.jpg" alt="" width="20%" height="20%"/>
+    <h2>Name : Queen Of Tears</h2>
+    <h3>Rating : 8.2</h3>
+    <p>Summary : Queen of Tears (Korean: 눈물의 여왕) is a 2024 South Korean romantic comedy television series written by Park Ji-eun, co-directed by Jang Young-woo [ko] and Kim Hee-won, and starring Kim Soo-hyun, Kim Ji-won, Park Sung-hoon, Kwak Dong-yeon, and Lee Joo-bin. The series explores the nuances of a married couple going through a difficult time together. It aired on tvN from March 9, to April 28, 2024, every Saturday and Sunday at 21:20 (KST). It is also available for streaming on TVING in South Korea, and on Netflix in selected regions.</p>
+  </div>
+  )
+}
